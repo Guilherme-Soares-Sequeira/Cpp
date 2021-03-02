231b16de99410afc261cf11864b0f14c219c0641
Guilherme-Soares-Sequeira <guilhermesoaressequeira@hotmail.com>
2021-03-02 23:46:54 +0000

    First exercise complete

---
+++ classExercises/1_01.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main() {
+    char aChar;
+    int aNum;
+    std::cout << "A char? ";
+    std::cin >> aChar;
+    aNum = aChar;
+    std::cout << aNum << std::endlB;
+    return 0;
+}
+
+
