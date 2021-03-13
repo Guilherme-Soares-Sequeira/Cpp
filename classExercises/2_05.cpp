f511c34477620b0160887a3e29e3f9203474d627
Guilherme-Soares-Sequeira <guilhermesoaressequeira@hotmail.com>
2021-03-13 02:35:56 +0000

    Finished it

--- classExercises/2_05.cpp
+++ classExercises/2_05.cpp
@@ -8,11 +8,11 @@ int  main() {
     float A, B, C;
     std::cin >> A >> B >> C;
     float sqrtOperand = B*B - 4 * A * C;
-    if (sqrtOperand == 0) std::cout << "The equation has 2 equal real roots: " << std::setw(5) << static_cast<float> (-B) / (2*A) << std::setfill('0') << std::endl;
-    else if (sqrtOperand > 0) std::cout << "The equation has 2 real roots: " << std::setw(5) << (-B + sqrt(sqrtOperand)) / (2.0 * A) << std::setfill('0') << " and " << (-B - sqrt(sqrtOperand)) / (2.0 * A) << std::setfill('0') << std::endl;
+    if (sqrtOperand == 0) std::cout << std::fixed << std::setprecision(3) << "The equation has 2 equal real roots: " <<  static_cast<float> (-B) / (2*A) << std::endl;
+    else if (sqrtOperand > 0) std::cout << std::fixed << std::setprecision(3) << "The equation has 2 real roots: " << (-B + sqrt(sqrtOperand)) / (2.0 * A) << " and " << (-B - sqrt(sqrtOperand)) / (2.0 * A) <<  std::endl;
     else {
         float realpart = (-B) / (2.0*A), complexpart = sqrt(-sqrtOperand) / (2.0*A);
-        std::cout << "The equation has 2 complex roots: " << std::setw(5) << realpart << std::setfill('0') <<  "+" << std::setw(5)<< complexpart << std::setfill('0') <<"i and " << std::setw(5) << realpart << std::setfill('0') << "-" << std::setw(5) << complexpart << std::setfill('0') << "i" << std::endl;
+        std::cout << std::fixed << std::setprecision(3) << "The equation has 2 complex roots: " << realpart << "+" << complexpart  << "i and " << realpart << "-" << complexpart << "i" << std::endl;
     }
     return 0;
 }
