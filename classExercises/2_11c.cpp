88e880f746b36fb85a50d76601af63fa0fbee457
Guilherme-Soares-Sequeira <guilhermesoaressequeira@hotmail.com>
2021-03-19 12:41:07 +0000

    Doesn't seem to be working...

---
+++ classExercises/2_11c.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+//Quick iterative factorial function, returns unsigned long long because factorial scales very fast
+unsigned long long int factorial(int num) {
+    if (num == 0) return 1;
+    else if (num == 1) return 1;
+
+    unsigned long long result = 1;
+    for (int i = num; i > 1; i--) {
+        result *= i;
+    }
+    return result;
+}
+
+
+int main() {
+    //More than 66 terms and overflow occurs
+    std::cout << "Please enter the number of terms (max supported is 66): ";
+    int terms;
+    std::cin >> terms;
+    std::cout << "Please enter the power of x (positive integer only): ";
+    unsigned int x;
+    std::cin >> x;
+    
+    if (terms <= 0) {
+        std::cout << "The sum of the first " << terms << " terms is " << 0 << std::endl;
+        return 0;
+    }
+    else if (terms == 1) {
+        std::cout << "The sum of the first " << terms << " terms is " << 1 << std::endl;
+        return 0;
+    }
+
+    //Trying to delay overflow and minimize inaccuracies
+    long double result = 1.0;
+    for (int i = 1; i < terms; i++) {
+        std::cout << "Sign: " << pow(-1, i) << " pow(" <<  x << "," << i << ") = " << pow(x,i) << " factorial(" << i << ") = " << factorial(i) << " total multiplication = " << (pow(-1, i) * static_cast<long double> (pow(x, i))/factorial(i)) << "\n";
+        result += (pow(-1, i) * static_cast<long double> (pow(x, i))/factorial(i));
+    }
+    std::cout << std::setprecision(15) << "The sum of the first " << terms << " terms is " << result << std::endl;
+    return 0;
+}
